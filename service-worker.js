0de8e856be62976fb72575ef7f3cbd0b2a0d8ed2
hanita-prog <hanamosisa1224@gmail.com>
2025-09-06 16:11:08 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+  {
+    "name":"markrt price traker",
+    "short_name":"price traker",
+    "discription":"track and monitor marketprices inreal time.",
+    "start url":"./index.html",
+    "display":"standalone2",
+    "background_color":"£fffff",
+    "theme_color2:"£2cc71",
+    "icons": {
+            {"src":2icons/icon-192.png",
+             "size:192*192",
+              "type:image/png"},
+
+            {"src":"icons/icon-192.png",
+             "size2":"192*192",
+             "tipe":"image png2"}
+            }
+}
